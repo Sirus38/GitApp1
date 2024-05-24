6057a214fafd891fc80b8cadbb66da90a501bdb5
Bera Agca <beraagca334@gmail.com>
2024-05-24 21:44:06 +0200

    v21

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -6,6 +6,9 @@ public class Runner {
         System.out.println("Hello GIT");
         System.out.println("git add . : staging areaya alir.");
 
+        //versiyon 2
+        System.out.println("git commit -m\"\" ile versiyon olusturulur");
+
 
     }
 }
